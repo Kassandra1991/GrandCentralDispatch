faf8b4dba90ec0602a552a5f2521ea019f45f4df
Alex Asichka <sanechka3@mail.ru>
2022-06-15 14:18:38 +0300

    Initial Commit

---
+++ GrandCentralDispatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GrandCentralDispatch
+//
+//  Created by MacBook Pro on 2022-06-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
