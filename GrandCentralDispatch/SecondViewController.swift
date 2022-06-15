9264301d319c2ab29e28f4ed31f6f765694aea2e
Alex Asichka <sanechka3@mail.ru>
2022-06-15 14:45:41 +0300

    update interface

---
+++ GrandCentralDispatch/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  GrandCentralDispatch
+//
+//  Created by MacBook Pro on 2022-06-15.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
