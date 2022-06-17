009c49e7a958c20cc70ff623132406b5076b34e5
Alex Asichka <sanechka3@mail.ru>
2022-06-17 12:15:39 +0300

    found upload trouble, add activityIndicator

--- GrandCentralDispatch/SecondViewController.swift
+++ GrandCentralDispatch/SecondViewController.swift
@@ -9,10 +9,33 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            activityIndicatorView.isHidden = true
+            activityIndicatorView.stopAnimating()
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchImage()
 
     }
 
-
+    func fetchImage() {
+        imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg")
+        activityIndicatorView.isHidden = false
+        activityIndicatorView.startAnimating()
+        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
+            self.image = UIImage(data: imageData)
+    }
 }
